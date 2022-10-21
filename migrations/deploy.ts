eb2998ea8f7a41fc775f2c876ed973138fc26659
Levani Tevdoradze <12980455+tevdoradze@users.noreply.github.com>
2022-10-21 16:32:03 +0200

    Add deploy script

--- migrations/deploy.ts
+++ migrations/deploy.ts
@@ -2,11 +2,64 @@
 // single deploy script that's invoked from the CLI, injecting a provider
 // configured from the workspace's Anchor.toml.
 
-const anchor = require("@project-serum/anchor");
+import * as anchor from "@project-serum/anchor";
+import { Program, AnchorProvider, Wallet } from "@project-serum/anchor";
+import { Casier } from "../target/types/casier";
+import {
+  PublicKey,
+  Connection,
+  Keypair,
+  SystemProgram,
+  SYSVAR_RENT_PUBKEY,
+} from "@solana/web3.js";
+import * as fs from "fs";
 
-module.exports = async function (provider) {
+function loadKeypair(keypairPath: string): any {
+  return <any>JSON.parse(fs.readFileSync(keypairPath).toString());
+}
+
+function loadWallet(keypair: string): Keypair {
+  return Keypair.fromSecretKey(new Uint8Array(loadKeypair(keypair)));
+}
+
+const fee_payer = loadWallet(process.env.FEE_PAYER_KEY_PATH);
+
+module.exports = async function () {
   // Configure client to use the provider.
-  anchor.setProvider(provider);
+  const idl = JSON.parse(
+    require("fs")
+      .readFileSync(`${process.cwd()}/../target/idl/casier.json`, "utf8")
+      .toString()
+  );
+  const connection = new Connection(
+    "https://aurory.rpcpool.com/76a89d061372798b1eab339287e7",
+    "recent"
+  );
+  const wallet = new Wallet(fee_payer);
+  const provider = new AnchorProvider(connection, wallet, {
+    commitment: "recent",
+  });
+  const program = new Program<Casier>(
+    idl,
+    new PublicKey("CAsieqooSrgVxhgWRwh21gyjq7Rmuhmo4qTW9XzXtAvW"),
+    provider
+  );
+  const tx1 = await program.methods.initialize().rpc();
+  console.log(tx1);
+  const [configPDA] = await PublicKey.findProgramAddress(
+    [anchor.utils.bytes.utf8.encode("config")],
+    program.programId
+  );
+  const tx2 = await program.methods
+    .initConfig()
+    .accounts({
+      config: configPDA,
+      feePayer: fee_payer.publicKey,
+      systemProgram: SystemProgram.programId,
+      rent: SYSVAR_RENT_PUBKEY,
+    })
+    .rpc();
+  console.log(tx2);
 
   // Add your deploy script here.
 };
